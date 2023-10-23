3f7c6c5f6f40e1688ce2ee00c06f33267ec689b4
unknown <you@example.com>
2023-10-23 20:14:40 -0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Container from "react-bootstrap/Container"
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar"
+import { Container, Nav, Navbar } from "react-bootstrap"
 import { LinkedIn, YouTube } from "@mui/icons-material"
 
-import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 
 import { Invocate } from "./components/Invocate"
